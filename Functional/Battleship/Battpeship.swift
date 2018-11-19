49682e06e7da09198801c238a31d59744b208d59
qishuaipeng <1425917930@qq.com>
2018-11-20 01:09:37 +0800

    2018.11.20凌晨

--- Functional/Battleship/Battpeship.swift
+++ Functional/Battleship/Battpeship.swift
@@ -30,6 +30,15 @@ struct Ship {
     var unsafeRange: Distance
 }
 
+extension Position {
+    func minus(tagert: Position) -> Position {
+        return Position(x: x - tagert.x, y: y - tagert.y)
+    }
+    func length() -> Distance {
+        return sqrt(x*x + y*y)
+    }
+}
+
 // MARK: - 扩展Ship类型
 extension Ship {
     /// 添加一个canEnageShip函数用于检查是否另一艘船在射程范围内
@@ -37,19 +46,39 @@ extension Ship {
     /// - Parameter target: 目标船
     /// - Returns: 是否在射程范围内
     func canEnageShip(target: Ship) -> Bool {
-        let dX: Distance = target.position.x - position.x
-        let dY: Distance = target.position.y - position.y
-        return sqrt(dX*dX + dY*dY) <= firingRange
+//        let dX: Distance = target.position.x - position.x
+//        let dY: Distance = target.position.y - position.y
+//        return sqrt(dX*dX + dY*dY) <= firingRange
+        
+        return position.minus(tagert: target.position).length() <= firingRange
     }
+    
+    /// 判断是否另一艘船在不安全范围内
+    ///
+    /// - Parameter target: 目标船
+    /// - Returns: 否另一艘船在不安全范围内
     func inUnsafeRange(target: Ship) -> Bool {
-        let dX: Distance = target.position.x - position.x
-        let dY: Distance = target.position.y - position.y
-        return sqrt(dX*dX + dY*dY) <= unsafeRange
+//        let dX: Distance = target.position.x - position.x
+//        let dY: Distance = target.position.y - position.y
+//        return sqrt(dX*dX + dY*dY) <= unsafeRange
+        
+        return position.minus(tagert: target.position).length() <= unsafeRange
     }
+    
+    /// 判断是否另一艘船在射程范围内并且在不安全范围外
+    ///
+    /// - Parameter target: 目标船
+    /// - Returns: 否另一艘船在射程范围内并且在不安全范围外
     func canSafelyEngageShip(target: Ship) -> Bool {
         return canEnageShip(target: target) && (!inUnsafeRange(target: target))
     }
-    func canSafelyEngageShip1(target: Ship, friend: Ship) -> Bool {
-        return canSafelyEngageShip(target: target) && friend.canSafelyEngageShip(target: target)
-    }
+}
+
+typealias Region = (Position) -> Bool
+
+func circle(center: Position, radius: Distance) -> Region {
+    return { point in center.minus(tagert: point).length() <= radius }
+}
+func shift(region: @escaping Region, offset: Position) -> Region {
+    return { point in region(Position(x: point.x + offset.x, y: point.y + offset.y)) }
 }
