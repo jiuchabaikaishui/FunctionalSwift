8b37c70c1326af0d8936f8de423aef1d8c8fbbe4
綦帅鹏 <qishuaipeng@qishuaipengdeiMac.local>
2018-11-26 20:22:49 +0800

    改改

--- Functional/ImageProcessing/ImageFilter.swift
+++ Functional/ImageProcessing/ImageFilter.swift
@@ -8,8 +8,13 @@
 import Foundation
 import CoreImage
 
+/// 图片滤镜
 typealias Filter = (CIImage) -> CIImage
 
+/// 高斯模糊滤镜
+///
+/// - Parameter radius: 半径
+/// - Returns: 高斯模糊滤镜
 func gaussianBlur(radius: Double) -> Filter {
     return { image in
         let parameters: [String: Any] = [kCIInputRadiusKey: radius, kCIInputImageKey: image]
@@ -18,4 +23,46 @@ func gaussianBlur(radius: Double) -> Filter {
         
         return outputImage
     }
+}
+
+/// 颜色生成滤镜
+///
+/// - Parameter color: 颜色
+/// - Returns: 颜色生成滤镜
+func colorGenerator(color: CIColor) -> Filter {
+    return { image in
+        let parameters: [String: Any] = [kCIInputColorKey: color]
+        guard let filter = CIFilter(name: "CIConstantColorGenerator", parameters: parameters) else { fatalError() }
+        guard let outputImage = filter.outputImage else { fatalError() }
+        
+//        print("---------\(image.extent)")
+        let enImage = outputImage.cropped(to: image.extent)
+//        print("---------\(enImage.extent)")
+        
+        return enImage
+    }
+}
+func compositeSourceOver(overLay: CIImage) -> Filter {
+    return { image in
+        let parameters: [String: Any] = [kCIInputBackgroundImageKey: image, kCIInputImageKey: overLay]
+        guard let filter = CIFilter(name: "CISourceOverCompositing", parameters: parameters) else { fatalError() }
+        guard let outputImage = filter.outputImage else { fatalError() }
+        
+        return outputImage
+    }
+}
+func colorOverlay(color: CIColor) -> Filter {
+    return { image in
+        let overLay = colorGenerator(color: color)(image)
+        return compositeSourceOver(overLay: overLay)(image)
+    }
+}
+func composeFilters(filter1: @escaping Filter, filter2: @escaping Filter) -> Filter {
+    return { image in
+        return filter2(filter1(image))
+    }
+}
+infix operator >>>: MultiplicationPrecedence
+func >>>(filter1: @escaping Filter, filter2: @escaping Filter) -> Filter {
+    return { image in filter2(filter1(image)) }
 }
--- Functional/ImageProcessing/ViewController.swift
+++ Functional/ImageProcessing/ViewController.swift
@@ -13,31 +13,70 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var imageV: UIImageView!
     @IBOutlet weak var scrollV: UIScrollView!
     @IBOutlet weak var resetB: UIButton!
+    var currentI: UIImage?
     var optionsB: Array<UIButton> = []
     
-    
+    @objc func gaussianBlurAction(sender: UIButton) {
+        let image = self.imageV.image!
+        let ciImage = (image.ciImage != nil) ? image.ciImage : CIImage(image: image)
+        
+        self.imageV.image = UIImage(ciImage: gaussianBlur(radius: 20)(ciImage!))
+    }
+    @objc func colorGeneratorAction(sender: UIButton) {
+        let image = self.imageV.image!
+        let ciImage = (image.ciImage != nil) ? image.ciImage : CIImage(image: image)
+        
+        print("++++++++++++\(self.imageV.image!.size)")
+        let endImage = UIImage(ciImage: colorGenerator(color: CIColor(color: UIColor.blue))(ciImage!))
+        print("------------\(endImage.size)")
+        
+        self.imageV.image = endImage
+    }
+    @objc func colorOverlayAction(sender: UIButton) {
+        let image = self.imageV.image!
+        let ciImage = (image.ciImage != nil) ? image.ciImage : CIImage(image: image)
+        
+        self.imageV.image = UIImage(ciImage: colorOverlay(color: CIColor(color: UIColor.green))(ciImage!))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let arr = ["高斯模糊"]
-        for str: String in arr {
-            let strObj = NSString(string: str)
+        let arr: [[String: Any]] = [["title": "高斯模糊", "selector": #selector(gaussianBlurAction(sender:))], ["title": "颜色覆盖", "selector": #selector(colorGeneratorAction(sender:))], ["title": "颜色叠加", "selector": #selector(colorOverlayAction(sender:))]]
+        for item in arr {
+            let title = item["title"] as! String
+            let selector = item["selector"] as! Selector
             let optionB = UIButton(type: UIButton.ButtonType.system)
-            optionB.setTitle(str, for: UIControl.State.normal)
-            optionB.frame = CGRect(x: self.resetB.frame.origin.x + self.resetB.frame.size.width + 8, y: self.resetB.frame.origin.y, width: strObj.boundingRect(with: CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: optionB.titleLabel!.font], context: nil).width, height: self.resetB.frame.size.height)
-            self.view.addSubview(optionB)
+            optionB.setTitle(title, for: UIControl.State.normal)
+           
+            optionB.addTarget(self, action: selector, for: UIControl.Event.touchUpInside)
+            let button: UIButton = self.optionsB.count > 0 ? self.optionsB.last! : self.resetB
+            let X = button.frame.origin.x + button.frame.size.width + 8
+            let strObj = NSString(string: title)
+            let W = strObj.boundingRect(with: CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: optionB.titleLabel!.font], context: nil).width
+            optionB.frame = CGRect(x: X, y: self.resetB.frame.origin.y, width: W, height: self.resetB.frame.size.height)
+            self.scrollV.addSubview(optionB)
             self.optionsB.append(optionB)
+            self.scrollV.contentSize = CGSize(width: W, height: self.scrollV.contentSize.height)
         }
+        self.currentI = imageV.image
         self.imageV.addObserver(self, forKeyPath: "image", options: NSKeyValueObservingOptions.new, context: nil)
     }
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if let _ = keyPath?.elementsEqual("image") {
-            if let _ = object {
-                self.imageV.backgroundColor = UIColor.darkText
-                self.resetB.isEnabled = true
-            } else {
+            let image = change?[NSKeyValueChangeKey.newKey] as? UIImage
+            if image == nil {
                 self.imageV.backgroundColor = UIColor.white
                 self.resetB.isEnabled = false
+                for button in self.optionsB {
+                    button.isEnabled = false
+                }
+            } else {
+                print("*************\(self.imageV.image!.size)")
+                self.imageV.backgroundColor = UIColor.black
+                self.resetB.isEnabled = !((self.currentI?.isEqual(image))!)
+                for button in self.optionsB {
+                    button.isEnabled = true
+                }
             }
         }
     }
@@ -49,7 +88,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     @IBAction func imageAction(_ sender: UIBarButtonItem) {
         let alert: UIAlertController = UIAlertController(title: "选择图片", message: nil, preferredStyle: UIAlertController.Style.actionSheet)
-        let cameraA: UIAlertAction = UIAlertAction(title: "相机", style: UIAlertAction.Style.default) { _ in
+        let cameraA: UIAlertAction = UIAlertAction(title: "相机", style: .default) { _ in
             let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
             if (status == AVAuthorizationStatus.restricted || status == AVAuthorizationStatus.denied) {//收限或拒绝
                 self.openSetting()
@@ -77,7 +116,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
         }
         alert.addAction(cameraA)
-        let photoA: UIAlertAction = UIAlertAction(title: "相册", style: UIAlertAction.Style.default) { _ in
+        let photoA: UIAlertAction = UIAlertAction(title: "相册", style: .default) { _ in
             let status: PHAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
             if (status == PHAuthorizationStatus.restricted || status == PHAuthorizationStatus.denied) {
                 self.openSetting()
@@ -99,14 +138,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         alert.addAction(photoA)
         if ((imageV?.image) != nil) {
-            let removeA: UIAlertAction = UIAlertAction(title: "移除", style: UIAlertAction.Style.default) { _ in
-                
+            let removeA: UIAlertAction = UIAlertAction(title: "移除", style: .default) { _ in
+                self.currentI = nil
+                self.imageV.image = nil
             }
             alert.addAction(removeA)
         }
+        let cancelA: UIAlertAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        alert.addAction(cancelA)
         self.present(alert, animated: true, completion: nil)
     }
     @IBAction func resetAction(_ sender: UIButton) {
+        self.imageV.image = self.currentI
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -115,6 +158,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         }
         
+        self.currentI = image
         self.imageV.image = image
         
         self.dismiss(animated: true, completion: nil)
