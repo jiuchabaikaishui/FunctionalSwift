d9c908957568eb813084dc7ddeb3d6b1e7d95d15
qishuaipeng <1425917930@qq.com>
2019-02-26 07:36:00 +0800

    修改一点点

--- Functional/ImageProcessing/ImageFilter.swift
+++ Functional/ImageProcessing/ImageFilter.swift
@@ -31,12 +31,12 @@ func gaussianBlur(radius: Double) -> Filter {
 /// - Parameter color: 颜色
 /// - Returns: 颜色生成滤镜
 func colorGenerator(color: CIColor) -> Filter {
-    return { _ in
+    return { image in
         let parameters: [String: Any] = [kCIInputColorKey: color]
         guard let filter = CIFilter(name: "CIConstantColorGenerator", parameters: parameters) else { fatalError() }
         guard let outputImage = filter.outputImage else { fatalError() }
         
-        return outputImage
+        return outputImage.cropped(to: image.extent)
     }
 }
 
@@ -60,7 +60,7 @@ func compositeSourceOver(overLay: CIImage) -> Filter {
 /// - Returns: 颜色叠层滤镜
 func colorOverlay(color: CIColor) -> Filter {
     return { image in
-        let overLay = colorGenerator(color: color)(image)
+        let overLay = colorGenerator(color: color)(image).cropped(to: CGRect(x: image.extent.size.width/4, y: image.extent.size.height/4, width: image.extent.width/2, height: image.extent.size.height/2))
         return compositeSourceOver(overLay: overLay)(image)
     }
 }
@@ -77,19 +77,16 @@ func combine() -> Filter {
 //        return overlaidImage;
 //    }
     
-    ///方案er：将上面两个滤镜调用表达式合并为一体
+    ///方案二：将上面两个滤镜调用表达式合并为一体
 //    return { image in
-//        let radius = 5.0
-//        let color = UIColor.red.ciColor
-//
 //        return colorOverlay(color: color)(gaussianBlur(radius: radius)(image))
 //    }
     
-    ///方案三：将上面两个滤镜调用表达式合并为一体
-//    return composeFilters(filter1: gaussianBlur(radius: radius), filter2: colorOverlay(color: color))
+    ///方案三：使用符合函数合并两个滤镜
+    return composeFilters(filter1: gaussianBlur(radius: radius), filter2: colorOverlay(color: color))
     
-    ///方案四：将上面两个滤镜调用表达式合并为一体
-    return gaussianBlur(radius: radius) >>> colorOverlay(color: color)
+    ///方案四：使用运算符组合两个滤镜
+//    return gaussianBlur(radius: radius) >>> colorOverlay(color: color)
 }
 func composeFilters(filter1: @escaping Filter, filter2: @escaping Filter) -> Filter {
     return { image in
@@ -117,62 +114,61 @@ var result2 = add2(x: 1)(2)
 struct Filter1 {
     let output : (CIImage) -> CIImage
 }
-extension Filter1 {
-    func gaussianBlur(radius: CGFloat) -> Filter1 {
-        return Filter1(output: { image in
-            let parameters: [String: Any] = [kCIInputRadiusKey: radius, kCIInputImageKey: image]
-            guard let filter = CIFilter(name: "CIGaussianBlur", parameters: parameters) else { fatalError() }
-            guard let outputImage = filter.outputImage else { fatalError() }
-            
-            return outputImage
-        })
-    }
-    func colorGenerator(color: CIColor) -> Filter1 {
-        return Filter1(output: { _ in
-            let parameters: [String: Any] = [kCIInputColorKey: color]
-            guard let filter = CIFilter(name: "CIConstantColorGenerator", parameters: parameters) else { fatalError() }
-            guard let outputImage = filter.outputImage else { fatalError() }
-            
-            return outputImage
-        })
-    }
-    func compositeSourceOver(image: CIImage) -> Filter1 {
-        return Filter1(output: { image in
-            let parameters: [String: Any] = [kCIInputBackgroundImageKey: image, kCIInputImageKey: image]
-            guard let filter = CIFilter(name: "CISourceOverCompositing", parameters: parameters) else { fatalError() }
-            guard let outputImage = filter.outputImage else { fatalError() }
-            
-            return outputImage.cropped(to: image.extent)
-        })
-    }
-    func colorOverlay(color: CIColor) -> Filter1 {
-        return Filter1(output: { image in
-            self.compositeSourceOver(image: self.colorGenerator(color: color).output(image)).output(image)
-        })
-    }
-    /// 组合滤镜
-    func combine() -> Filter1 {
-        let radius: CGFloat = 5.0
-        let color = UIColor.red.ciColor
-        ///方案一：将滤镜应用于图像
-//        return Filter1(output: { image in
-//            let blur = self.gaussianBlur(radius: radius)
-//            let colorO = self.colorOverlay(color: color)
-//
-//            return colorO.output(blur.output(image))
-//        })
+func gaussianBlur1(radius: CGFloat) -> Filter1 {
+    return Filter1(output: { image in
+        let parameters: [String: Any] = [kCIInputRadiusKey: radius, kCIInputImageKey: image]
+        guard let filter = CIFilter(name: "CIGaussianBlur", parameters: parameters) else { fatalError() }
+        guard let outputImage = filter.outputImage else { fatalError() }
         
-        ///方案二：将上面两个滤镜调用表达式合并为一体
-//        return Filter1(output: { image in self.colorOverlay(color: color).output(self.gaussianBlur(radius: radius).output(image))})
+        return outputImage
+    })
+}
+func colorGenerator1(color: CIColor) -> Filter1 {
+    return Filter1(output: { _ in
+        let parameters: [String: Any] = [kCIInputColorKey: color]
+        guard let filter = CIFilter(name: "CIConstantColorGenerator", parameters: parameters) else { fatalError() }
+        guard let outputImage = filter.outputImage else { fatalError() }
         
-        ///方案三：将上面两个滤镜调用表达式合并为一体
-//        return self.composeFilters(filter1: self.gaussianBlur(radius: radius), filter2: self.colorOverlay(color: color))
+        return outputImage
+    })
+}
+func compositeSourceOver1(image: CIImage) -> Filter1 {
+    return Filter1(output: { backImage in
+        let parameters: [String: Any] = [kCIInputBackgroundImageKey: backImage, kCIInputImageKey: image]
+        guard let filter = CIFilter(name: "CISourceOverCompositing", parameters: parameters) else { fatalError() }
+        guard let outputImage = filter.outputImage else { fatalError() }
         
-        ///方案四：将上面两个滤镜调用表达式合并为一体
-        return self.gaussianBlur(radius: radius) *** self.colorOverlay(color: color)
-    }
+        return outputImage.cropped(to: image.extent)
+    })
+}
+func colorOverlay1(color: CIColor) -> Filter1 {
+    return Filter1(output: { image in
+        compositeSourceOver1(image: colorGenerator1(color: color).output(image).cropped(to: CGRect(x: image.extent.size.width/4, y: image.extent.size.height/4, width: image.extent.width/2, height: image.extent.size.height/2))).output(image)
+    })
+}
+/// 组合滤镜
+func combine() -> Filter1 {
+    let radius: CGFloat = 5.0
+    let color = UIColor.red.ciColor
+    ///方案一：将滤镜应用于图像
+//    return Filter1(output: { image in
+//        let blur = gaussianBlur1(radius: radius).output(image)
+//        let colorOver = colorOverlay1(color: color).output(blur)
+//        return colorOver
+//    })
     
-    func composeFilters(filter1: Filter1, filter2: Filter1) -> Filter1 {
+    ///方案二：将上面两个滤镜调用表达式合并为一体
+//    return Filter1(output: { image in colorOverlay1(color: color).output(gaussianBlur1(radius: radius).output(image)) })
+    
+    ///方案三：使用符合函数合并两个滤镜
+//    return composeFilters1(filter1: gaussianBlur1(radius: radius), filter2: colorOverlay1(color: color))
+    
+    ///方案四：使用运算符组合两个滤镜
+    return gaussianBlur1(radius: radius) *** colorOverlay1(color: color)
+}
+
+extension Filter1 {
+    func composeFilters1(filter1: Filter1, filter2: Filter1) -> Filter1 {
         return Filter1(output: { image in filter1.output(filter2.output(image)) })
     }
 }
