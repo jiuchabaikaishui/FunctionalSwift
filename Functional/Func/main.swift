77b51c4842059cdbf1290f29f3d22adaee691db2
QSP <QSP@163.com>
2021-08-10 17:59:24 +0800

    小修改

--- Functional/Func/main.swift
+++ Functional/Func/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-indirect enum BinarySearchTree<Element: Equatable>  {
+indirect enum BinarySearchTree<Element: Comparable>  {
     case leaf
     case node(BinarySearchTree<Element>, Element, BinarySearchTree<Element>)
 }
@@ -23,9 +23,7 @@ extension BinarySearchTree {
     init(value: Element) {
         self = .node(.leaf, value, .leaf)
     }
-}
-
-extension BinarySearchTree {
+    
     var count: Int {
         switch self {
         case .leaf:
@@ -34,9 +32,7 @@ extension BinarySearchTree {
             return left.count + 1 + right.count
         }
     }
-}
-
-extension BinarySearchTree {
+    
     var elements: Array<Element> {
         switch self {
         case .leaf:
@@ -45,9 +41,7 @@ extension BinarySearchTree {
             return left.elements + [value] + right.elements
         }
     }
-}
-
-extension BinarySearchTree {
+    
     func recuce<A>(initialResult: A, nextNodeResult: (A, Element, A) -> A) -> A {
         switch self {
         case .leaf:
@@ -63,4 +57,70 @@ extension BinarySearchTree {
     func elements1() -> Array<Element> {
         self.recuce(initialResult: []) { $0 + [$1] + $2 }
     }
+    
+    var isEmpty: Bool {
+        if case .leaf = self {
+            return true
+        }
+        return false
+    }
+    
+    var isBST: Bool {
+        switch self {
+        case .leaf:
+            return true
+        case let .node(left, value, right):
+            return left.isBST && right.isBST && left.elements.reduce(false, { $0 && $1 < value }) && right.elements.reduce(false, { $0 && $1 > value })
+        }
+    }
+    
+    func contains(value: Element) -> Bool {
+        switch self {
+        case .leaf:
+            return false
+        case let .node(_, myValue, _) where value == myValue:
+            return true
+        case let .node(left, myValue, _) where value > myValue:
+            return left.contains(value: value)
+        case let .node(_, myValue, right) where value < myValue:
+            return right.contains(value: value)
+        default:
+            return false
+        }
+    }
+    func contains1(value: Element) -> Bool {
+        switch self {
+        case .leaf:
+            return false
+        case let .node(left, myValue, right):
+            if value == myValue {
+                return true
+            } else if value > myValue {
+                return left.contains1(value: value)
+            } else {
+                return right.contains1(value: value)
+            }
+        }
+    }
+    
+    mutating func insert(value: Element) {
+        switch self {
+        case .leaf:
+            self = BinarySearchTree(value: value)
+        case .node(var left, let myValue, var right):
+            if value > myValue {
+                left.insert(value: value)
+            } else if value < myValue {
+                right.insert(value: value)
+            }
+            self = .node(left, myValue, right)
+        }
+    }
 }
+
+var tree = BinarySearchTree(value: 10)
+var tree1 = tree
+tree1.insert(value: 8)
+print(tree.elements)
+print(tree1.elements)
+
